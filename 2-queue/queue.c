e55bcfc6afa68dc0cc1f53bbbae49fe6b5a64ee4
hyaldiz <huseyinyaldizother@protonmail.com>
2023-02-16 17:51:36 +0300

    queue update

---
+++ 2-queue/queue.c
@@ -0,0 +1,83 @@
+#include "queue.h"
+
+HndQueue_t createQueue(size_t initialCapacity)
+{
+    HndQueue_t pQueue = (HndQueue_t)malloc(sizeof(Queue_t));
+    if(!pQueue)
+    {
+        fprintf(stderr,"Queue handler için yeterli alan tahsis edilemedi!\n");
+        return NULL;
+    }
+
+    pQueue->pItems = (QueueValueType_t*)malloc(sizeof(QueueValueType_t)*initialCapacity);
+    if(!pQueue->pItems)
+    {
+        fprintf(stderr,"Queue memberlar için yeterli alan tahsis edilemedi!\n");
+        free(pQueue);
+    }
+
+    pQueue->capacity = initialCapacity;
+    pQueue->size = 0;
+    pQueue->front = -1;
+    pQueue->rear = -1;
+
+    return pQueue;
+}
+
+size_t queueSize(HndQueue_t queue)
+{
+    return queue->size;
+}
+
+bool isFull(HndQueue_t queue)
+{
+    if(queue->rear == queue->capacity -1)
+        return true;
+    return false;
+}
+
+bool isEmpty(HndQueue_t queue)
+{
+    if(queue->rear == -1)
+        return true;
+    return false;
+}
+
+void enQueue(HndQueue_t queue,QueueValueType_t newValue)
+{
+    if(isFull(queue))
+        fprintf(stderr,"enQueue full\n");
+    else
+    {
+        if(queue->front == -1)
+            queue->front = 0;
+        queue->rear++;
+        queue->pItems[queue->rear] = newValue;
+        queue->size++;
+    }
+}
+
+void deQueue(HndQueue_t queue)
+{
+    if(isEmpty(queue))
+        fprintf(stderr,"deQueue Empty!\n");
+    else
+    {
+        queue->front++;
+        queue->size--;
+        if(queue->front > queue->rear)
+            queue->front = queue->rear = -1;        
+    }
+}
+
+void printQueue(HndQueue_t queue)
+{
+    if(isEmpty(queue))
+        fprintf(stderr,"printQueue empty!\n");
+    else
+    {
+        for(int index = queue->front;index <= queue->rear;index++)
+            printf("%d",queue->pItems[index]);
+        printf("\n");
+    }
+}
--- 2-queue/queue.h
+++ 2-queue/queue.h
@@ -48,86 +48,12 @@ typedef struct Queue
     size_t size;
 }*HndQueue_t,Queue_t;
 
-HndQueue_t createQueue(size_t initialCapacity)
-{
-    HndQueue_t pQueue = (HndQueue_t)malloc(sizeof(Queue_t));
-    if(!pQueue)
-    {
-        fprintf(stderr,"Queue handler için yeterli alan tahsis edilemedi!\n");
-        return NULL;
-    }
-
-    pQueue->pItems = (QueueValueType_t*)malloc(sizeof(QueueValueType_t)*initialCapacity);
-    if(!pQueue->pItems)
-    {
-        fprintf(stderr,"Queue memberlar için yeterli alan tahsis edilemedi!\n");
-        free(pQueue);
-    }
-
-    pQueue->capacity = initialCapacity;
-    pQueue->size = 0;
-    pQueue->front = -1;
-    pQueue->rear = -1;
-
-    return pQueue;
-}
-
-size_t queueSize(HndQueue_t queue)
-{
-    return queue->size;
-}
-
-bool isFull(HndQueue_t queue)
-{
-    if(queue->rear == queue->capacity -1)
-        return true;
-    return false;
-}
-
-bool isEmpty(HndQueue_t queue)
-{
-    if(queue->rear == -1)
-        return true;
-    return false;
-}
-
-void enQueue(HndQueue_t queue,QueueValueType_t newValue)
-{
-    if(isFull(queue))
-        fprintf(stderr,"enQueue full\n");
-    else
-    {
-        if(queue->front == -1)
-            queue->front = 0;
-        queue->rear++;
-        queue->pItems[queue->rear] = newValue;
-        queue->size++;
-    }
-}
-
-void deQueue(HndQueue_t queue)
-{
-    if(isEmpty(queue))
-        fprintf(stderr,"deQueue Empty!\n");
-    else
-    {
-        queue->front++;
-        queue->size--;
-        if(queue->front > queue->rear)
-            queue->front = queue->rear = -1;        
-    }
-}
-
-void printQueue(HndQueue_t queue)
-{
-    if(isEmpty(queue))
-        fprintf(stderr,"printQueue empty!\n");
-    else
-    {
-        for(int index = queue->front;index <= queue->rear;index++)
-            printf("%d",queue->pItems[index]);
-        printf("\n");
-    }
-}
+HndQueue_t createQueue(size_t initialCapacity);
+size_t queueSize(HndQueue_t queue);
+bool isFull(HndQueue_t queue);
+bool isEmpty(HndQueue_t queue);
+void enQueue(HndQueue_t queue,QueueValueType_t newValue);
+void deQueue(HndQueue_t queue);
+void printQueue(HndQueue_t queue);
 
 #endif
